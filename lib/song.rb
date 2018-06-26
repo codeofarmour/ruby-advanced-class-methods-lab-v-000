4ff8fe76559ee2b5174cdf6623ecde8081d8d4a1
Terri Riggle <codeofarmour@outlook.com>
2018-06-26 03:40:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,5 +51,14 @@ class Song
     song 
   end
   
-  
+  def self.create_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+    
+    song = self.create 
+    song.name = song_name
+    song.artist_name = artist_name
+    song 
+  end 
 end
